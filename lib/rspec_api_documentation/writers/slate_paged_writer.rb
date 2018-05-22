dc371449159fe6f269e34fd8006d532d091e5c67
Ellie Peterson <ekp2h92@gmail.com>
2018-05-22 14:35:36 -0400

    Cleaned up some code, edited slate_paged_head.mustache to reflect latest slate paged changes.

--- lib/rspec_api_documentation/writers/slate_paged_writer.rb
+++ lib/rspec_api_documentation/writers/slate_paged_writer.rb
@@ -1,8 +1,7 @@
 module RspecApiDocumentation
   module Writers
     class SlatePagedWriter < MarkdownWriter
-      EXTENSION = 'html.md'
-      FILENAME = 'index'
+      FILENAME = 'index.html.md'
 
       def self.clear_docs(docs_dir)
         FileUtils.mkdir_p(docs_dir)
@@ -31,12 +30,13 @@ module RspecApiDocumentation
 
       def write
         File.open(configuration.docs_dir
-         .join("#{FILENAME}.#{extension}"), "w+") do |f|
+         .join("#{FILENAME}"), "w+") do |f|
           f.write markup_index_head_class.new(index, configuration).render
           f.write markup_head_class.new(index, configuration).render
           f.write markup_index_class.new(index, configuration).render
           IndexHelper.sections(index.examples, @configuration).each do |section|
             f.write "# #{section[:resource_name]}\n\n"
+            f.write "#{section[:resource_explanation]}\n\n"
           end
         end
         write_example
@@ -47,25 +47,17 @@ module RspecApiDocumentation
           markup_example = markup_example_class.new(example, configuration)
           FileUtils.mkdir_p(configuration.docs_dir.join(markup_example.resource_dir_name))
 
-          File.open(configuration.docs_dir
-           .join(markup_example.resource_dir_name, "#{FILENAME}.#{extension}"), "w+") do |f|
+          File.open(configuration.docs_dir.join(markup_example.resource_dir_name, "#{FILENAME}"), "w+") do |f|
             f.write markup_example_head_class.new(example, configuration).render
             f.write markup_head_class.new(index, configuration).render
-            IndexHelper
-              .sections(index.examples, @configuration).each do |section|
-
-              f.write "# #{section[:resource_name]}\n\n"
-              f.write "#{section[:resource_explanation]}\n\n"
+            IndexHelper.sections(index.examples, @configuration).each do
               markup_example = markup_example_class.new(example, configuration)
               f.write markup_example.render
             end
           end
         end
       end
-
-      def extension
-        EXTENSION
-      end
+      
     end
   end
 end
